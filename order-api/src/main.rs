32da4657e81180f03103fe9321eafa525643586a
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2026-02-25 17:18:03 -0800

    Get a starting worker running

---
+++ order-api/src/main.rs
@@ -0,0 +1,12 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(async || "Orders server is running!"));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    println!("Listening on http://0.0.0.0:8080");
+
+    axum::serve(listener, app).await.unwrap();
+}
